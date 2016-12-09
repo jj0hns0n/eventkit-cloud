73a0dec725fa214289a313a84e9f5ba058acff82
Michael Van Veen <michael@mvanveen.net>
2016-12-09 14:10:52 -0800

    move chown over to Docker file, try ls -l

--- scripts/unit-test-entrypoint.sh
+++ scripts/unit-test-entrypoint.sh
@@ -7,7 +7,7 @@ export WORKON_HOME=/var/lib/eventkit/.virtualenvs
 export PROJECT_HOME=/var/lib/eventkit
 
 env
-ls 
+ls -l
 ls eventkit_cloud 
 ls eventkit_cloud/settings/
 
@@ -17,5 +17,4 @@ ls eventkit_cloud/settings/
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/insert_provider_types.json
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/osm_provider.json
 
-#chown -R eventkit:eventkit /var/log/eventkit /var/lib/eventkit
 /var/lib/eventkit/.virtualenvs/eventkit/bin/python manage.py test eventkit/
