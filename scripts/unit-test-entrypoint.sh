5684ac491cc36b3b5252fb598510900592b95dd5
Michael Van Veen <michael@mvanveen.net>
2017-01-09 13:50:05 -0800

    path moved from /var/lib/eventkit/.virtualenvs to /var/lib/.virtualenvs

--- scripts/unit-test-entrypoint.sh
+++ scripts/unit-test-entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd /var/lib/eventkit
-source /var/lib/eventkit/.virtualenvs/eventkit/bin/activate
+source /var/lib/.virtualenvs/eventkit/bin/activate
 
 unset PRODUCTION
 export DATABASE_URL=postgis://eventkit:eventkit_exports@postgis:5432/eventkit_exports
 export BROKER_URL=amqp://guest:guest@rabbitmq:5672/
 export C_FORCE_ROOT=True
 export EXPORT_DOWNLOAD_ROOT=/var/lib/eventkit/exports_download
-export WORKON_HOME=/var/lib/eventkit/.virtualenvs
+export WORKON_HOME=/var/lib/.virtualenvs
 export PROJECT_HOME=/var/lib/eventkit
 
-/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py collectstatic --noinput
-/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py makemigrations
-/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py migrate
-/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/admin_user.json
+/var/lib/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py collectstatic --noinput
+/var/lib/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py makemigrations
+/var/lib/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py migrate
+/var/lib/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/admin_user.json
 #/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/insert_provider_types.json
 #/var/lib/eventkit/.virtualenvs/eventkit/bin/python /var/lib/eventkit/manage.py loaddata /var/lib/eventkit/eventkit_cloud/fixtures/osm_provider.json
 
